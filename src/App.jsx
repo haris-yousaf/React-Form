e70cd2dce78eb5c1b743c0154f878ff1d4b43994
Haris Yousaf <154906048+haris-yousaf@users.noreply.github.com>
2024-07-27 15:02:14 +0500

    React form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PaymentPage from './PaymentPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PaymentPage />
+    </div>
+  );
+}
+
+export default App;
